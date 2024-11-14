ef3c94ff80c88753ecf5b1219c1c8542f0b315f9
sulem <sulem477@gmail.com>
2024-11-14 16:48:55 -0600

    feat: restclient

--- controllers/productos.controller.js
+++ controllers/productos.controller.js
@@ -1,4 +1,4 @@
-const { producto, categoria, Sequelize } = require('../models')
+const { producto, categoria, Sequelize, archivo } = require('../models')
 const { body, param, validationResult } = require('express-validator')
 const Op = Sequelize.Op
 
@@ -9,34 +9,40 @@ self.productoGetValidator = [
 ]
 
 self.productoValidator = [
-    body('titulo', `El campo {0} es obligatorio`).not().isEmpty().isLength({ max: 255 }),
-    body('descripcion', 'El campo {0} es obligatorio').not().isEmpty().isLength({ max: 65535}),
-    body('precio', 'El campo {0} es obligatorio').not().isEmpty().isDecimal({ force_decimal: false})
+    body('titulo', `El campo titulo es obligatorio`).not().isEmpty().isLength({ max: 255 }),
+    body('descripcion', 'El campo descripcion es obligatorio').not().isEmpty().isLength({ max: 65535 }),
+    body('precio', 'El campo precio es obligatorio').not().isEmpty().isDecimal({ force_decimal: false }),
+    body('archivoid')
+        .optional()
+        .isInt().withMessage("El campo archivoid no es válido")
 ]
 
 self.productoPutValidator = [
     param('id', 'Es obligatorio un ID numérico').not().isEmpty().isInt(),
-    body('titulo', `El campo {0} es obligatorio`).not().isEmpty().isLength({ max: 255 }),
-    body('descripcion', 'El campo {0} es obligatorio').not().isEmpty().isLength({ max: 65535}),
-    body('precio', 'El campo {0} es obligatorio').not().isEmpty().isDecimal({ force_decimal: false})
+    body('titulo', `El campo titulo es obligatorio`).not().isEmpty().isLength({ max: 255 }),
+    body('descripcion', 'El campo descripcion es obligatorio').not().isEmpty().isLength({ max: 65535 }),
+    body('precio', 'El campo precio es obligatorio').not().isEmpty().isDecimal({ force_decimal: false }),
+    body('archivoid')
+        .optional()
+        .isInt().withMessage("El campo archivoid no es válido")
 ]
 
 self.productoCategoriaPostValidator = [
     param('id', 'Es obligatorio un ID numérico').not().isEmpty().isInt(),
-    body('categoriaid', `El campo {0} es obligatorio`).not().isEmpty().isInt()
+    body('categoriaid', `El campo categoriaid es obligatorio`).not().isEmpty().isInt()
 ]
 
 self.productoCategoriaDeleteValidator = [
     param('id', 'Es obligatorio un ID numérico').not().isEmpty().isInt(),
-    param('categoriaid', `El campo {0} es obligatorio`).not().isEmpty().isInt()
+    param('categoriaid', `El campo categoriaid es obligatorio`).not().isEmpty().isInt()
 ]
 
-self.getAll = async function (req, res, next){
+self.getAll = async function (req, res, next) {
     try {
-        const {s} = req.query
+        const { s } = req.query
 
         const filters = {}
-        if(s) {
+        if (s) {
             filters.titulo = {
                 [Op.like]: `%${s}`
             }
@@ -49,14 +55,14 @@ self.getAll = async function (req, res, next){
                 model: categoria,
                 as: 'categorias',
                 attributes: [['id', 'categoriaId'], 'nombre', 'protegida'],
-                through: { attributes: []}
+                through: { attributes: [] }
             },
             subQuery: false
         })
 
         return res.status(200).json(data)
 
-    } catch(error){
+    } catch (error) {
         next(error)
     }
 }
@@ -64,7 +70,7 @@ self.getAll = async function (req, res, next){
 self.get = async function (req, res, next) {
     try {
         const errors = validationResult(req)
-        if(!errors.isEmpty()) throw new Error(JSON.stringify(errors))
+        if (!errors.isEmpty()) throw new Error(JSON.stringify(errors))
 
         let id = req.params.id
         let data = await producto.findByPk(id, {
@@ -73,16 +79,16 @@ self.get = async function (req, res, next) {
                 model: categoria,
                 as: 'categorias',
                 attributes: [['id', 'categoriaId'], 'nombre', 'protegida'],
-                through: {attributes: []}
+                through: { attributes: [] }
             }
         })
 
-        if(data)
+        if (data)
             res.status(200).json(data)
         else
             res.status(404).send()
 
-    } catch(error){
+    } catch (error) {
         next(error)
     }
 }
@@ -90,19 +96,26 @@ self.get = async function (req, res, next) {
 self.create = async function (req, res, next) {
     try {
         const errors = validationResult(req)
-        if(!errors.isEmpty()) throw new Error(JSON.stringify(errors))
+        if (!errors.isEmpty()) throw new Error(JSON.stringify(errors))
+
+        const archivoid = req.body.archivoid
+        if (archivoid) {
+            let archivodata = await archivo.findByPk(req.body.archivoid)
+            if (!archivodata)
+                return res.status(404).send()
+        }
 
         let data = await producto.create({
             titulo: req.body.titulo,
             descripcion: req.body.descripcion,
             precio: req.body.precio,
-            archivoid: req.body.archivoid || null
+            archivoid: archivoid || null
         })
 
         //req.bitacora("producto.crear", data.id)
         res.status(201).json(data)
 
-    } catch(error){
+    } catch (error) {
         next(error)
     }
 }
@@ -110,18 +123,25 @@ self.create = async function (req, res, next) {
 self.update = async function (req, res, next) {
     try {
         const errors = validationResult(req)
-        if(!errors.isEmpty()) throw new Error(JSON.stringify(errors))
+        if (!errors.isEmpty()) throw new Error(JSON.stringify(errors))
+
+        const archivoid = req.body.archivoid
+        if (archivoid) {
+            let archivodata = await archivo.findByPk(req.body.archivoid)
+            if (!archivodata)
+                return res.status(404).send()
+        }
 
         let id = req.params.id
         let body = req.body
-        let data = await producto.update(body, {where: { id: id }})
-        if(data[0] === 0)
+        let data = await producto.update(body, { where: { id: id } })
+        if (data[0] === 0)
             return res.status(404).send()
 
         //req.bitacora("producto.editar", id)
         res.status(204).send()
 
-    } catch(error){
+    } catch (error) {
         next(error)
     }
 }
@@ -129,20 +149,20 @@ self.update = async function (req, res, next) {
 self.delete = async function (req, res, next) {
     try {
         const errors = validationResult(req)
-        if(!errors.isEmpty()) throw new Error(JSON.stringify(errors))
+        if (!errors.isEmpty()) throw new Error(JSON.stringify(errors))
 
         let id = req.params.id
-        let data = await producto.findByPk(id)   
-        if(!data)
+        let data = await producto.findByPk(id)
+        if (!data)
             return res.status(404).send()
 
-        data = await producto.destroy({where: { id: id}})
-        if( data === 1){
+        data = await producto.destroy({ where: { id: id } })
+        if (data === 1) {
             //req.bitacora("producto.eliminar", id)
             return res.status(204).send()
         }
         res.status(404).send()
-    } catch(error){
+    } catch (error) {
         next(error)
     }
 }
@@ -150,20 +170,20 @@ self.delete = async function (req, res, next) {
 self.asignaCategoria = async function (req, res, next) {
     try {
         const errors = validationResult(req)
-        if(!errors.isEmpty()) throw new Error(JSON.stringify(errors))
+        if (!errors.isEmpty()) throw new Error(JSON.stringify(errors))
 
         let itemToAssign = await categoria.findByPk(req.body.categoriaid)
-        if(!itemToAssign) return res.status(404).send()
+        if (!itemToAssign) return res.status(404).send()
 
         let item = await producto.findByPk(req.params.id)
-        if(!item) return res.status(404).send()
+        if (!item) return res.status(404).send()
 
         await item.addCategoria(itemToAssign)
 
         //req.bitacora("productocategoria.agregar", `${req.params.id}:${req.body.categoriaid}`)
         res.status(204).send()
 
-    } catch(error){
+    } catch (error) {
         next(error)
     }
 }
@@ -171,20 +191,20 @@ self.asignaCategoria = async function (req, res, next) {
 self.eliminaCategoria = async function (req, res, next) {
     try {
         const errors = validationResult(req)
-        if(!errors.isEmpty()) throw new Error(JSON.stringify(errors))
-        
+        if (!errors.isEmpty()) throw new Error(JSON.stringify(errors))
+
         let itemToRemove = await categoria.findByPk(req.params.categoriaid)
-        if(!itemToRemove) return res.status(404).send()
+        if (!itemToRemove) return res.status(404).send()
 
         let item = await producto.findByPk(req.params.id)
-        if(!item) return res.status(404).send()
+        if (!item) return res.status(404).send()
 
         await item.removeCategoria(itemToRemove)
 
         //req.bitacora("productocategoria.remover", `${req.params.id}:${req.body.categoriaid}`)
         res.status(204).send()
 
-    } catch(error){
+    } catch (error) {
         next(error)
     }
 }
