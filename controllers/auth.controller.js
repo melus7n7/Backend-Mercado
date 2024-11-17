04d9f1d31a98e02fe28a8ce5c7d658c29619af7b
JJacobMS <montielsalasjesus@gmail.com>
2024-11-17 13:29:50 -0600

    AuthController

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,2 +1,46 @@
 const bcrypt = require('bcryt')
 const {usuario, rol, Sequelize} = require('../models')
+const {GeneraToken, TiempoRestanteToken} = require('../services/jwttoken.service')
+
+let self = {}
+
+self.login = async function(req, res, next) {
+    const {email, password} = req.body
+
+    try{
+        let data = await usuario.findOne({
+            where: {email: email},
+            raw: true,
+            attributes: ['id','email','nombre','passwordhash',[Sequelize.col('rol.nombre'),'rol']],
+            include: {model:rol, attributes:[]}
+        })
+        if(data===null){
+            return res.status(401).json({mensaje:'Usuario o contraseña incorrectos'})
+        }
+
+        const passwordMash = await bcrypt.compare(password,data.passwordhash)
+        if(!passwordMash){
+            return res.status(401).json({mensaje:'Usuario o contraseña incorrectos'})
+        }
+        token = GeneraToken(data.email, data.nombre, data.rol)
+        req.bitacora("usuario.login",data.email)
+        res.status(200).json({
+            email: data.email,
+            nombre: data.nombre,
+            rol:data.rol,
+            jwt:token
+        })
+    } catch(error){
+        next(error)
+    }
+}
+
+self.tiempo = async function (req,res){
+    const tiempo = TiempoRestanteToken(req)
+    if(tiempo == null){
+        res.status(404).send()
+    }
+    res.status(200).send()
+}
+
+module.exports = self
