cadbaede41db2c221212505aae55127e16a89ea1
sulem <sulem477@gmail.com>
2024-11-22 00:50:07 -0600

    fix: carrito.controller

--- controllers/carritos.controller.js
+++ controllers/carritos.controller.js
@@ -69,7 +69,6 @@ self.getDetails = async function (req, res, next) {
         const errors = validationResult(req)
         if (!errors.isEmpty()) throw new Error(JSON.stringify(errors))
 
-        //Conseguir por el usuario
         let carritoRecuperado = await obtenerCarritoUsuario(req);
         if (carritoRecuperado == null) {
             return res.status(400).send("No existe el carrito");
@@ -78,7 +77,12 @@ self.getDetails = async function (req, res, next) {
 
         let data = await carritoproducto.findAll({
             attributes: ['cantidad', ['productoid', 'productoId']],
-            where: { productoid: req.params.idProducto, carritoid: carritoid }
+            where: { productoid: req.params.idProducto, carritoid: carritoid },
+            include: {
+                model: producto,
+                as: 'producto',
+                attributes: ['titulo']
+            }
         })
 
         if (data)
