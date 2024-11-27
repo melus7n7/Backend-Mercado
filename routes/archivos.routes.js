427a6994383475c4e04629d6bc4747f624c5e265
sulem <sulem477@gmail.com>
2024-11-27 01:19:40 -0600

    fix: delete files in system storage

--- routes/archivos.routes.js
+++ routes/archivos.routes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 const archivos = require('../controllers/archivos.controller')
 const Authorize = require('../middlewares/auth.middleware')
-const upload = require('../middlewares/upload.middleware')
+const {uploadFile, customFilename} = require('../middlewares/upload.middleware')
 
 router.get('/', Authorize('Administrador'), archivos.getAll)
 router.get('/:id', archivos.validarArchivoID, archivos.get)
 router.get('/:id/detalle', Authorize('Administrador'), archivos.validarArchivoID, archivos.getDetalle)
-router.post('/', upload.single('file'), Authorize('Administrador'), archivos.validarDatosArchivo, archivos.create)
-router.put('/:id', upload.single("file"), Authorize('Administrador'), archivos.validarDatosIDArchivo, archivos.update)
+router.post('/', uploadFile.single('file'),customFilename, Authorize('Administrador'), archivos.validarDatosArchivo, archivos.create)
+router.put('/:id', uploadFile.single("file"),customFilename, Authorize('Administrador'), archivos.validarDatosIDArchivo, archivos.update)
 router.delete("/:id", Authorize('Administrador'), archivos.validarArchivoID, archivos.delete)
 
 module.exports = router
