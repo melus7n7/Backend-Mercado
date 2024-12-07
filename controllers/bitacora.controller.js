03759a36be6a35c73c125af2f3d62cbcc75b066f
sulem <sulem477@gmail.com>
2024-12-06 19:44:24 -0600

    fix: auth tiempo y bitacora

--- controllers/bitacora.controller.js
+++ controllers/bitacora.controller.js
@@ -3,11 +3,16 @@ const {bitacora} = require('../models')
 let self = {}
 
 self.getAll = async function (req, res, next) {
-    let data = await bitacora.findAll({
-        attributes: [['id','bitacoraId'],'accion','elementoid','ip','usuario','fecha'],
-        order:[['id','DESC']]
-    })
-    res.status(200).json(data)
+    try{
+        let data = await bitacora.findAll({
+            attributes: [['id','bitacoraId'],'accion','elementoid','ip','usuario','fecha'],
+            order:[['id','DESC']]
+        })
+        res.status(200).json(data)
+    }catch(error){
+        next(error)
+    }
+    
 }
 
 module.exports = self
